3e05459f3ebff6601e2b1c6d49afc614852164ac
Long Nguyen <ngdlong91@gmail.com>
2022-01-04 15:22:40 +0700

    Rm warn for prod build

--- apps/indexer/lib/indexer/empty_blocks_sanitizer.ex
+++ apps/indexer/lib/indexer/empty_blocks_sanitizer.ex
@@ -17,7 +17,7 @@ defmodule Indexer.EmptyBlocksSanitizer do
 
   alias Explorer.{Chain, Repo}
   alias Explorer.Chain.Block
-  alias Explorer.Chain.Import.Runner.Blocks
+#  alias Explorer.Chain.Import.Runner.Blocks
 
   # milliseconds
   @timeout 1_000
@@ -111,34 +111,34 @@ defmodule Indexer.EmptyBlocksSanitizer do
     )
   end
 
-  defp set_is_empty_for_block(block_hash) do
-    query =
-      from(
-        block in Block,
-        select: block,
-        where: block.hash == ^block_hash,
-        lock: "FOR UPDATE"
-      )
-
-    updated_at = DateTime.utc_now()
-
-    update_query =
-      from(
-        b in Block,
-        join: s in subquery(query),
-        on: b.hash == s.hash,
-        update: [
-          set: [
-            is_empty: true,
-            updated_at: ^updated_at
-          ]
-        ],
-        select: s
-      )
-
-    Repo.update_all(update_query, [], timeout: @timeout)
-  rescue
-    postgrex_error in Postgrex.Error ->
-      {:error, %{exception: postgrex_error}}
-  end
+#  defp set_is_empty_for_block(block_hash) do
+#    query =
+#      from(
+#        block in Block,
+#        select: block,
+#        where: block.hash == ^block_hash,
+#        lock: "FOR UPDATE"
+#      )
+#
+#    updated_at = DateTime.utc_now()
+#
+#    update_query =
+#      from(
+#        b in Block,
+#        join: s in subquery(query),
+#        on: b.hash == s.hash,
+#        update: [
+#          set: [
+#            is_empty: true,
+#            updated_at: ^updated_at
+#          ]
+#        ],
+#        select: s
+#      )
+#
+#    Repo.update_all(update_query, [], timeout: @timeout)
+#  rescue
+#    postgrex_error in Postgrex.Error ->
+#      {:error, %{exception: postgrex_error}}
+#  end
 end
