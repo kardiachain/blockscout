54834d0d9700c0734f47ac25de0469e9734125aa
Long Nguyen <ngdlong91@gmail.com>
2022-01-04 16:29:46 +0700

    Update supervisor

--- apps/indexer/lib/indexer/supervisor.ex
+++ apps/indexer/lib/indexer/supervisor.ex
@@ -12,7 +12,7 @@ defmodule Indexer.Supervisor do
     CalcLpTokensTotalLiqudity,
 #    EmptyBlocksSanitizer,
     PendingOpsCleaner,
-    #PendingTransactionsSanitizer,
+    PendingTransactionsSanitizer,
     SetAmbBridgedMetadataForTokens,
     SetOmniBridgedMetadataForTokens
   }
@@ -129,7 +129,7 @@ defmodule Indexer.Supervisor do
       {CoinBalanceOnDemand.Supervisor, [json_rpc_named_arguments]},
       #{EmptyBlocksSanitizer, [[json_rpc_named_arguments: json_rpc_named_arguments]]},
       {TokenTotalSupplyOnDemand.Supervisor, [json_rpc_named_arguments]},
-#      {PendingTransactionsSanitizer, [[json_rpc_named_arguments: json_rpc_named_arguments]]},
+      {PendingTransactionsSanitizer, [[json_rpc_named_arguments: json_rpc_named_arguments]]},
 
       # Temporary workers
       {UncatalogedTokenTransfers.Supervisor, [[]]},
