7a0acda2c3f7f28c42a3cd30b12b87f498f9d6f9
Long Nguyen <ngdlong91@gmail.com>
2022-01-10 16:49:30 +0700

    Update receipt fetcher

--- apps/indexer/lib/indexer/block/fetcher/receipts.ex
+++ apps/indexer/lib/indexer/block/fetcher/receipts.ex
@@ -24,7 +24,7 @@ defmodule Indexer.Block.Fetcher.Receipts do
       {:ok, {:ok, %{logs: logs, receipts: receipts}}}, {:ok, %{logs: acc_logs, receipts: acc_receipts}} ->
         {:cont, {:ok, %{logs: acc_logs ++ logs, receipts: acc_receipts ++ receipts}}}
 
-      {:ok, {:error, reason}}, {:ok, acc} ->
+      {:ok, {:error, _reason}}, {:ok, acc} ->
         {:cont, {:ok, acc}}
 
         # Look like RPC call error,
