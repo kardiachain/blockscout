50f4a01a285a2e64410a51995919412531400564
longnd <ngdlong91@gmail.com>
2022-05-27 15:51:24 +0700

    Update policy

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -13,8 +13,8 @@ defmodule BlockScoutWeb.CSPHeader do
       "content-security-policy" => "\
         connect-src 'self' #{websocket_endpoints(conn)} https://raw.githubusercontent.com/kardiachain/token-assets/;\
         default-src 'self';\
-        script-src 'self' https://www.google.com https://www.gstatic.com;\
-        style-src 'self' https://fonts.googleapis.com;\
+        script-src 'self' 'sha256-IBxcGAt2latSKF1Je/SHpNwTeU5Q1WKiBVNd13xlAiA=' 'sha256-fqGNTzWau5hfYKKH3B59dBNOSh8VdaIQ5YXzH+9C0Ys=' 'sha256-89/1LLiXXHmqi3EdeHqEQ7Kz1VVc+6OXCUaen1kiUfg=' 'sha256-whIQUF4JEIB+ZBZ/ml/I0GDURCAMg5P2mSWr3aE/knc='  https://www.google.com https://www.gstatic.com;\
+        style-src 'self' 'sha256-N6tSydZ64AHCaOWfwKbUhxXx2fRFDxHOaL3e3CO7GPI=' 'sha256-biLFinpqYMtWHmXfkA1BPeCY0/fNt46SAZ+BBk5YUog=' https://fonts.googleapis.com;\
         img-src 'self' * data:;\
         media-src 'self' * data:;\
         font-src 'self' https://fonts.gstatic.com data:;\
