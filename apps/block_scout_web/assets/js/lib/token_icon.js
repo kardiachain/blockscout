be287c249074737155d68dff944aefaf9ecbac25
longnd <ngdlong91@gmail.com>
2022-10-12 15:25:25 +0700

    Update assets

--- apps/block_scout_web/assets/js/lib/token_icon.js
+++ apps/block_scout_web/assets/js/lib/token_icon.js
@@ -62,17 +62,17 @@ function getTokenIconUrl (chainID, addressHash) {
   return `https://raw.githubusercontent.com/kardiachain/token-assets/master/tokens/${addressHash}/logo.png`
 }
 
-function appendTokenIcon ($tokenIconContainer, chainID, addressHash, foreignChainID, foreignAddressHash, displayTokenIcons, size) {
+function appendTokenIcon ($tokenIconContainer, chainID, addressHash, displayTokenIcons, size) {
   const iconSize = size || 20
-  var tokenIconURL = null
-  tokenIconURL = getTokenIconUrl(null, addressHash)
+  const tokenIconURL = getTokenIconUrl(chainID.toString(), addressHash)
   if (displayTokenIcons) {
     checkLink(tokenIconURL)
       .then(checkTokenIconLink => {
         if (checkTokenIconLink) {
           if ($tokenIconContainer) {
-            var img = new Image(iconSize, iconSize)
+            const img = new Image(iconSize, iconSize)
             img.src = tokenIconURL
+            img.className = 'mr-1'
             $tokenIconContainer.append(img)
           }
         }
