b2f968a79c6a9c4c51f336befaa4404e9311c537
longnd <ngdlong91@gmail.com>
2022-10-14 23:46:27 +0700

    Update

--- apps/explorer/priv/repo/migrations/20221014111656_add_kardia_block_info.exs
+++ apps/explorer/priv/repo/migrations/20221014111656_add_kardia_block_info.exs
@@ -4,10 +4,10 @@ defmodule Explorer.Repo.Migrations.AddKardiaBlockInfo do
   def change do
     alter table(:blocks) do
       add(:block_rewards, :numeric, precision: 100, null: true)
-      add(:commit_hash, references(:addresses, column: :hash, type: :bytea), null: true)
-      add(:validator_hash, references(:addresses, column: :hash, type: :bytea), null: true)
-      add(:next_validator_hash, references(:addresses, column: :hash, type: :bytea), null: true)
-      add(:transaction_root, references(:addresses, column: :hash, type: :bytea), null: true)
+      add(:commit_hash, type: :bytea, null: true)
+      add(:validator_hash, type: :bytea, null: true)
+      add(:next_validator_hash, type: :bytea, null: true)
+      add(:transactions_root, type: :bytea, null: true)
     end
   end
 end
